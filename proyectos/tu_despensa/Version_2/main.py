d033987174be1eb1ff69a6a5cd244a5a94c4fcf3
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-11-23 11:40:56 -0600

    The code of the 'main.py' file in the path 'proyectos/tu_despensa/version_2/main.py' was documented

--- proyectos/tu_despensa/Version_2/main.py
+++ proyectos/tu_despensa/Version_2/main.py
@@ -15,7 +15,7 @@
 #Agregamos un poco de estilo al programa para que hacer más agradable lo que el usuario ve en su consola
 
 
-print(" ")
+print(" ") #Estás líneas de código generan un espacio vacio en la consola al momento de mostrar contenido al usuario
 
 print((" " * 40),"TuListApp")
 
@@ -24,8 +24,10 @@ print(" ")
 print((" " * 20),"¡Bienvenido a la mejor herramienta para crear listas de supermercado!")
 print((" " * 5),"Vamos a crear tu lista del super, para hacerlo solo tienes que seguir las instrucciones")
 
-print("")
+print(" ")
 
+
+#Mostramos en consola la pregunta '¿Qué desas hacer?' y le mostramos dos opciones posibles
 print("¿Qué deseas hacer?")
 print("")
 print("Ingresa el número 1 para crear una lista")
@@ -33,33 +35,44 @@ print("Ingresa el número 2 para salir")
 
 print(" ")
 
-eleccion = int(input("Ingresa tu eleción: "))
+eleccion = int(input("Ingresa tu eleción: ")) #Solicitamos al usuario que elija una de las opciones dadas, después usamos el metodo 'int' para covertir la respuesta del usuario en un valor numerico que almacenamos en la variable 'eleccion' 
 
 print(" ")
 
 
+#Usamos un 'if' para controlar el flujo del programa. "Si la variable elección es exactamente igual a 1 el programa debería ejecutar las siguientes instrucciones: "
 if eleccion == 1:
     
-    productos = []
+    productos = [] #Crear una lista vacia y almacenarla en la variable 'productos'
 
-    print(" ")
+    print(" ") #Imprimir un espacio vacio en consola
 
-    sig = input("¿Listo para ingresar tu primer item? (si), (no): ")
+    sig = input("¿Listo para ingresar tu primer item? (si), (no): ") #Solicitar el usuario que ingrese en formato string una de las opciones disponibles. (si) para ingresar el primer item, (no) para no realizar la acción mencionada y se almacena la respuesta del usuario en la variable 'sig'.
 
-    print(" ")
+    print(" ") #Imprimir un espacio vacio en consola
 
-    while sig == "si":
-        item = input("¿Qué necesitas comprar?: ")
-        productos.append(item)
+    while sig == "si": #Ejecutar un ciclo 'while', donde mientras 'sig' sea exactamente igual a "si" el ciclo while se ejecutara como verdadero y se realizan a su vez las siguientes acciones:
+
+        item = input("¿Qué necesitas comprar?: ") #Se pide al usuario que ingres el nombre del artículo o producto que desea ingresar a la lista, el cuál se almacena en la variable 'item'
+        productos.append(item) #Usamos el metodo 'append' para agregar el valor almacenado en 'item' a la lista productos
         print(" ")
 
-        print("Tu lista:")
-        print(productos)
+        print("Tu lista:") 
+        print(productos) #imprimimos los valores contenidos en la lista 'productos'
 
         print(" ")
-        sig = input("¿Ingresar nuevo item? (si), (no)?:  ")
-elif eleccion == 2:
-    print("Hasta luego, vuelve pronto")
-    exit()
+        sig = input("¿Ingresar nuevo item? (si), (no)?:  ") #Preguntamos al usuario si desea ingresar un nuevo artículo y asignamos este nuevo valor a la variable 'sig' para que sea evaluada de nueva cuenta por el 'while'
+elif eleccion == 2: #En caso de que la variable 'eleccion' sea exactamnet igual a 2, se ejecutan las siguientes elecciones:
+    print("Hasta luego, nos vemos en tu proxima lista") #Se imprime en consola un mensaje de despedida 
+    exit() #Se cierra el programa
 else:
-    print("ingresa una opción valida")
+    print("ingresa una opción valida") #Si en la primera ejecución de la variable 'eleccion' (que pregunta al usuario que desea hacer) se ingresa un valor numérico diferente a 1 o 2, el programa indica al usuario que necesita ingresar un valor valido y cierra el programa 
+
+#________________________________________________________________________________________________________
+
+#MEJORAR EL PROGRAMA (Lista las siguientes mejoras que necesitan hacerse en el programa):
+
+#En esta version el programa se cierra cuando el usuario ingresa una opción numerica diferente de 1 y 2, lo que obliga al usuario a ter que ejecutar las instrucciones desde 0. El programa tambien se detiene al momento de recibir valores string produciendo un error y cerrendo el programa, entonces...
+
+# 1.- El programa debería poder reiniciarse automaticamente en caso de que el usuario se equivoque al ingresar un valor no numerico.
+# 2.- El programa debería poder reiniciarse automaticamnte en caso de que el usuario ingrese un valor númerico no contemplado como opción valida.
