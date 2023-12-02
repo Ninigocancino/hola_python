db2b024d6e11ccff2ef8f41473681ff03a94ab10
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-02 13:25:43 -0600

    Code was documented in 'archivo_csv.py'.

--- Ejercicios/guardar_datos_desde_python/archivo_csv.py
+++ Ejercicios/guardar_datos_desde_python/archivo_csv.py
@@ -1,15 +1,24 @@
-import csv
+#------------------------------------Guardar datos en CSV usando python----------------------------------
 
-with open("alumnos.csv", "w", newline="") as archivo_csv:
-    escritor_csv = csv.writer(archivo_csv)
+#Ejemplo 01: Como convertir datos ingresados desde el usuario a través de input
 
-    encabezados = ["Nombre", "Grupo", "Genero"]
-    escritor_csv.writerow(encabezados)
+import csv #Importar la libreria CSV
 
-    nombre = input("Agrega tu nombre: ")
+with open("alumnos.csv", "w", newline="") as archivo_csv: #Usar 'with open' para abrir un archivo csv con permisos de escritura. Donde 'alumnos.csv' será el nombre del archivo, 'w' indica el permiso de escritura y 'newline=""' permnite crear una nueva linea en el archivo
+    escritor_csv = csv.writer(archivo_csv) # Se asigna a la variable 'escritor_csv' los permisos para escribir valores en el archivo
+
+    encabezados = ["Nombre", "Grupo", "Genero"] #Creamos la variable encabezados listar los nombres de los encabezados de las columnas, si es que el archivo debe contenerlo 
+    escritor_csv.writerow(encabezados) #La variable 'escritor_csv' escribe en la primera fila del archivo el contenido de la variable 'encabezado' es decir; los encabezados de columna
+
+
+    #Se crean variables para recibir los valores que se almacenaran en el archivo csv 
+    nombre = input("Agrega tu nombre: ") 
     grupo = input("Agrega el grupo al que perteneces: ")
     genero = input("Agrega tu genero: ")
 
-    escritor_csv.writerow([nombre, grupo, genero])
+    escritor_csv.writerow([nombre, grupo, genero]) #Volvemos a usar el metodo '.writerow' en 'escritor_csv' para agregar los valores de las variables 'nombre', 'grupo' y 'genero' por debajo de la fila del encabezado
 
-    archivo_csv.close()
+    archivo_csv.close() #Usamos el metodo '.close' para cerrar el programa después de que los datos se escriban
+    
+
+
