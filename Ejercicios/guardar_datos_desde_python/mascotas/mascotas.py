811bd2634b6a411f043af654ef4ea2c77cfcf7dc
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-05 21:54:51 -0600

    Se agrego una versión alternativa del ejercicio 'mascota.py'

--- Ejercicios/guardar_datos_desde_python/mascotas/mascotas.py
+++ Ejercicios/guardar_datos_desde_python/mascotas/mascotas.py
@@ -1,5 +1,6 @@
 import csv
 
+"""
 with open('mascotas.csv', 'w', newline='') as archivo_csv:
     escritor_csv = csv.writer(archivo_csv)
 
@@ -13,5 +14,29 @@ with open('mascotas.csv', 'w', newline='') as archivo_csv:
     escritor_csv.writerow([n_mascota,especie_m,duenio_m])
 
     print("se creo un archivo CSV")
+
+    archivo_csv.close()
+"""
+
+with open('datos_mascotas.csv', 'w', newline='') as archivo_csv:
+    escritor_csv = csv.writer(archivo_csv)
+
+    encabezado = ['Nombre_mascota', 'Especie', 'Dueño']
+
+    escritor_csv.writerow(encabezado)
+
+    while True:
+        nombre_m = input("Nombre de tu mascota: ")
+        especie_m = input("Especie de tu mascota:")
+        duenio = input("Tu nombre: ")
+
+        escritor_csv.writerow([nombre_m,especie_m,duenio])
+
+        print("se agrego una nueva lienea en datos_mascotas.csv")
+
+        respuesta = input("¿Quieres agregar una nueva línea de datos? s/n:  ").lower()
+
+        if respuesta != "s":
+            break
 
     archivo_csv.close()
