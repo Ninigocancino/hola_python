7f285deb5fcf53c73f7eba6b64c8a6cfca0a10e2
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-06 10:03:03 -0600

    Se agrego una nueva versión de código en el ejercicio 'frutas_verduras_dulces' y se creo (desde el código del programa) el archivo 'frut_verd_dulc.csv'

--- Ejercicios/guardar_datos_desde_python/frutas_verduras_dulces/frutas_verduras_dulces.py
+++ Ejercicios/guardar_datos_desde_python/frutas_verduras_dulces/frutas_verduras_dulces.py
@@ -4,6 +4,7 @@
 
 import csv
 
+"""
 with open('frutas_verduras_dulces.csv', 'w', newline='') as archivo_csv:
     escritor_csv = csv.writer(archivo_csv)
 
@@ -16,4 +17,31 @@ with open('frutas_verduras_dulces.csv', 'w', newline='') as archivo_csv:
 
     escritor_csv.writerow([frutas,verduras,dulces])
 
+    archivo_csv.close()
+
+    """
+
+#En la primeraversion de este código solo podías ingresar una nueva línea de datos justo al momento de crear el archivo '.csv'. El código siguiente estabasado en el anterior pero ahora utiliza un ciclo 'while' para iterar el código las veces que el usuario necesite para ingresar tantos datos quiera.
+
+
+with open('frut_verd_dulc.csv', 'w',newline='') as archivo_csv:
+
+    escritor_csv= csv.writer(archivo_csv)
+
+    encabezados=["Fruta","Verdura","Dulce"]
+    escritor_csv.writerow(encabezados)
+
+    while True:
+        fruta = input("Agrega una fruta: ")
+        verdura= input("Agrega una verdura: ")
+        dulce = input("Agrega un dulce: ")
+
+        escritor_csv.writerow([fruta,verdura,dulce])
+
+        print("Se agrego una nueva línea de datos")
+
+        respuesta = input("¿Quieres agregar más datos? s/n : ").lower()
+
+        if respuesta!= "s":
+            break
     archivo_csv.close()
