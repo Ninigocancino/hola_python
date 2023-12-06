b79a2cb85da9db3dd8ac649fa22bbc965ad8f906
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-06 10:31:44 -0600

    Se agrego nueva versión de código en el ejercicio 'datos_personales.py' y se creo, desde la ejecución del código el archivo 'data_personal.py'

--- Ejercicios/guardar_datos_desde_python/datos_personales/datos_personales.py
+++ Ejercicios/guardar_datos_desde_python/datos_personales/datos_personales.py
@@ -4,6 +4,7 @@
 
 import csv 
 
+"""
 with open('datos_personales.csv', 'w', newline='')as archivo_csv:
     escritor_csv = csv.writer(archivo_csv)
 
@@ -15,5 +16,29 @@ with open('datos_personales.csv', 'w', newline='')as archivo_csv:
     dni = input('ingresa tu DNI: ')
 
     escritor_csv.writerow([nombre,edad, dni])
+
+    archivo_csv.close()
+"""
+
+
+with open('data_personal.csv', 'w', newline='') as archivo_csv:
+    escritor_csv = csv.writer(archivo_csv)
+
+    header = ["Nombre", "Estado_civil", "Ciudad de residencia"]
+    escritor_csv.writerow(header)
+
+    while True: 
+        nombre = input("Agrega tu nombre: ")
+        est_civil = input("Agrega tu estado civil: ")
+        ciudad_natal = input("Agrega la ciudad donde naciste: ")
+
+        escritor_csv.writerow([nombre,est_civil,ciudad_natal])
+
+        print("Se agrego una nueva fila de datos al archivo 'data_personal.csv' ")
+
+        respuesta = input("¿Quieres agregar una nueva fila de datos? s/n: ").lower()
+
+        if respuesta != "s":
+            break
 
     archivo_csv.close()
