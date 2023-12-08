cf9412d6430d90bb8a6fb3130307ebf0e58b49a7
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-08 15:58:54 -0600

    Se agrego el archivo 'utilidades.py' al directorio 'version_8'

---
+++ proyectos/tu_despensa/version_8/utilidades.py
@@ -0,0 +1,8 @@
+import unicodedata
+
+def quitar_tildes(input_str):
+
+    #Elimina las tildes deuna cadena de caracteres utiliza la función unicodedata.normalize para convertir el string a su forma de composición canónica (NFKD), y luego utiliza una comprensión de lista para filtrar los caracteres que son marcadores de combinación (diacríticos)
+
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return ''.join([c for c in nfkd_form if not unicodedata.combining(c)])
