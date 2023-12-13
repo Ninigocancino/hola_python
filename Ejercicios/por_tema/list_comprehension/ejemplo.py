a03a713f333c60fe6d38a4805a4ca67217a409d0
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-12 22:50:59 -0600

    Se movio la carpeta 'guardar_datos_desde_python' a la recien creada 'por_caso_de_uso' y se creo la carpeta 'por_tema' done se agrego la carpeta 'list_comprehension' que contiene el fichero 'ejemplo.py'

---
+++ Ejercicios/por_tema/list_comprehension/ejemplo.py
@@ -0,0 +1,19 @@
+#List comprehension básico
+
+"""
+
+ciudades_mx = ["Ciudad de México", "Monterrey", "Villahermosa","Guadalajara", "Merida", "Veracruz"]
+
+mayusculas = [i.upper() for i in ciudades_mx]
+
+print(mayusculas)
+
+"""
+
+
+
+cifras = [23,45,98, 102,48, 12, 22, 12, 9, 89, 120, 406, 1002]
+
+numeros = [ i for i in cifras if i > 800]
+
+print(numeros)
