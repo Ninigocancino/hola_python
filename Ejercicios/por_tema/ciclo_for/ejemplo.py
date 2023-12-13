9b1842c882f11e3c4f4a070290684450fd1c365d
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-12 23:20:06 -0600

    Se creó el directorio 'ciclo_for' y se agregó el fichero 'ejemplo.py'

---
+++ Ejercicios/por_tema/ciclo_for/ejemplo.py
@@ -0,0 +1,18 @@
+#Ciclo for
+
+#Estructura básica
+
+"""
+colores = ["Rojo", "Blanco", "Azul"]
+
+for i in colores:
+    if i == "Rojo":
+        i = i.upper()
+        print(i)
+
+"""
+
+
+for i in range(0,10):
+    if i % 2 == 0 and i >:
+        print(i)
