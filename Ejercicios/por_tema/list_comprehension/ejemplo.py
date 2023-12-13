736c4e804bfd5b7259842c064de11a3779d75cd1
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-12 22:55:17 -0600

    Se agregaron indicaciones en el fichero 'ejemplo.py'

--- Ejercicios/por_tema/list_comprehension/ejemplo.py
+++ Ejercicios/por_tema/list_comprehension/ejemplo.py
@@ -2,6 +2,8 @@
 
 """
 
+#Ejemplo: Trabajar con listas que contienen caracteres
+
 ciudades_mx = ["Ciudad de México", "Monterrey", "Villahermosa","Guadalajara", "Merida", "Veracruz"]
 
 mayusculas = [i.upper() for i in ciudades_mx]
@@ -11,6 +13,7 @@ print(mayusculas)
 """
 
 
+#Ejemplo: Trabajar con listas que contienen valores númericos 
 
 cifras = [23,45,98, 102,48, 12, 22, 12, 9, 89, 120, 406, 1002]
 
