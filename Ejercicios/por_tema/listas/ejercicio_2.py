c31702a8748cdb84ab2a14a26f299df7298f732b
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-19 22:37:50 -0600

    Se agrego el segundo ejercicio del directorio en el archivo 'ejercicio_2.py'

---
+++ Ejercicios/por_tema/listas/ejercicio_2.py
@@ -0,0 +1,7 @@
+#Crea una lista con 10 nombres de personajes famosos e imprime el nombre de la posición 5
+
+personajes = ["Goku", "Woody", "Lilo", "Stich", "Zimba", "Naruto", "Seiya", "El chavo", "Luke Skywalker", "Picachu"]
+
+personaje = personajes[5]
+
+print(personaje)
