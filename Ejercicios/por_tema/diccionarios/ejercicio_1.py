5d4772214fbe7911ac92ab0128234ca993a959e8
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-19 22:54:46 -0600

    Nuevo directorio 'diccionarios': Se creó el directorio 'diccionarios' que contendrá diferentes eejrcicios relacionados con el tema de diccionarios en pthon. También se agregó elŕimer ejercicio en el archivo 'ejercicio_1.py'

---
+++ Ejercicios/por_tema/diccionarios/ejercicio_1.py
@@ -0,0 +1,10 @@
+#Crea un diccionario que contenga la información personal de una persona
+
+datos_personales = {
+    "Nombre": "Goku",
+    "Edad_Anios": 32,
+    "Ciudad_natal":"Desconocida",
+    "Esposa": "Milk"
+}
+
+print(datos_personales)
