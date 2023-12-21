49eb41f143ab28a45dd93af63a32e0878ee28a46
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-12-20 21:36:19 -0600

    ejercicio 02: Se agregó el archivo 'ejercicio_02.py' a la carpeta 'diccionarios'. Se escribio un diccionario con datos de una pelicula en el archivo

---
+++ Ejercicios/por_tema/diccionarios/ejercicio_02.py
@@ -0,0 +1,10 @@
+#Crea una lista con datos de una pelicula
+
+datos_peliculas = {
+    "Titulo" : "Avatar",
+    "Director" : "George Lucas",
+    "Genero" : "Ciencia Ficción",
+    "Año_Estreno" : "2009"
+}
+
+print(datos_peliculas)
