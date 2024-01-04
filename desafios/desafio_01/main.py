54ee74d72307e4c80e66715bb6f136d73e31733c
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2024-01-03 23:10:01 -0600

    Se agregó descripción del desafío 01 y se escribio el primer texto a imprimir en pantalla en el archivo 'main.py'

---
+++ desafios/desafio_01/main.py
@@ -0,0 +1,16 @@
+"""
+Luis y Luisa están planeando su boda, ellos consideran que no es necesario gastar tanto en una gran fiesta y desean ahorrar lo más posible. Mientras realizaban la planeación se dieron cuenta que podían ahorrarse mucho si el numero de invitados era lo más cercano posible al número de asistentes finales.
+
+Desafío:
+
+Crea un programa que muestre a los invitados un mensaje de invitación a la boda de Luis y Luisa con la información necesaria sobre la boda y después le pida confirmar si asistirán a la boda y que registre sus datos para agregarlos a la lista de invitados.
+
+Al final Luis y Luisa deben contar con la lista de invitados que confirmaron su asistencia.
+
+Genera 10 registros para confirmar que el programa funciona.
+"""
+
+print("                             Luis & Luisa                            ")
+print("")
+
+print("            Quermos que seas parte de nuestro enlace nupcial          ")
