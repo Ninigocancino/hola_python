b0521580f7e29ce0b64af7055c1be31936f316f5
VictorCancino <juanvictorcancino@gmail.com>
2024-03-11 19:07:53 -0600

    Se completo el ejercicio 2 en el fichero 'crear_listas.py'

--- Ejercicios/por_tema/listas/crear_listas.py
+++ Ejercicios/por_tema/listas/crear_listas.py
@@ -1,5 +1,15 @@
+"""
 #Ejercicio 1: Crea una lista con 5 números e imprime la lista después
 
 lista_numeros = [1,2,3,4,5]
 
 print(lista_numeros)
+
+"""
+
+#Ejercicio 2: Crea una lista con 10 personajes de anime e imprime el elemento en la posición 6
+
+personajes_anime = ["Naruto", "Seiya", "Goku", "Bulma", "Vegeta", "Krilim", "Sazuke", "Luffy","Zoro", "Astro"]
+
+print(personajes_anime[6])
+
