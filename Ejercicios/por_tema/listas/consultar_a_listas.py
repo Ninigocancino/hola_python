bfd0c6745bad306952de9fb85e3457f4789a2488
VictorCancino <juanvictorcancino@gmail.com>
2024-03-12 11:30:10 -0600

    Se completo el ejercicio 3 en el fichero 'consultar_a_listas.py'

--- Ejercicios/por_tema/listas/consultar_a_listas.py
+++ Ejercicios/por_tema/listas/consultar_a_listas.py
@@ -1,12 +1,16 @@
-#Ejercicio 1: Crea una lista con 10 profesiones y trae el valor que este en la posición 8
-
 profesiones = ["Arquitecto","Doctor","Programador","Diseñador","Dentista","Abogado", "Fisico", "Matematico","Deportista","Chef"]
+
+#Ejercicio 1: De la lista profesiones trae el valor que este en la posición 8
 
 print(profesiones[8])
 
-#Ejercicio 2: de la lista anterior  elige un valor e imprime el indice en el que se encuentra
+#Ejercicio 2: De la lista profesiones elige un valor e imprime el indice en el que se encuentra
 
 indice= profesiones.index("Doctor")
 
 print(indice)
+
+#Ejercicio 3: De la lista profesiones elige los valores que esten entre la posición 2 y la 8 y muestralos en la salidad de la terminal
+
+print(profesiones[2:8])
 
