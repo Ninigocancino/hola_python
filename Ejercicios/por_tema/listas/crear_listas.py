0f688e3b6438ba61f928b9a7527b6491a8f471d1
VictorCancino <juanvictorcancino@gmail.com>
2024-03-11 19:10:17 -0600

    Se completo el ejercicio 3 en el fichero 'crear_listas.py'

--- Ejercicios/por_tema/listas/crear_listas.py
+++ Ejercicios/por_tema/listas/crear_listas.py
@@ -7,9 +7,20 @@ print(lista_numeros)
 
 """
 
+"""
 #Ejercicio 2: Crea una lista con 10 personajes de anime e imprime el elemento en la posición 6
 
 personajes_anime = ["Naruto", "Seiya", "Goku", "Bulma", "Vegeta", "Krilim", "Sazuke", "Luffy","Zoro", "Astro"]
 
 print(personajes_anime[6])
+"""
 
+#Ejercicio 3: Crea una lista con 6 ciudades imprime todo los elementos de a lista y luego imprime solo el elemento en la posición 3
+
+ciudades = ["Bogota", "México", "Lima", "Río de Janeiro", "Caracas", "Buenos Aires"]
+
+print(ciudades)
+
+print("")
+
+print(ciudades[5])
