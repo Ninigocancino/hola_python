e087f38d7991e9b8d1c53e6a2bd3cda834a9db81
VictorCancino <juanvictorcancino@gmail.com>
2024-03-11 19:16:19 -0600

    Se completo el ejercicio 4 en el fichero 'crear_listas.py'

--- Ejercicios/por_tema/listas/crear_listas.py
+++ Ejercicios/por_tema/listas/crear_listas.py
@@ -15,6 +15,7 @@ personajes_anime = ["Naruto", "Seiya", "Goku", "Bulma", "Vegeta", "Krilim", "Saz
 print(personajes_anime[6])
 """
 
+"""
 #Ejercicio 3: Crea una lista con 6 ciudades imprime todo los elementos de a lista y luego imprime solo el elemento en la posición 3
 
 ciudades = ["Bogota", "México", "Lima", "Río de Janeiro", "Caracas", "Buenos Aires"]
@@ -24,3 +25,17 @@ print(ciudades)
 print("")
 
 print(ciudades[5])
+
+"""
+
+#Ejercicio 4: Crea una lista con 8 nombre de animales imprime todos los elementos de la lista, luego selecciona el elemento en la posición 7 y usalo para completar el mensaje "Tenemos un .... en la lista"
+
+animales = ["Perro", "Gato", "Cocodrilo", "Tarantula", "Garza","Pato", "Rana", "Mono"]
+
+elegido = animales[7]
+
+print(animales)
+
+print("")
+
+print(f"Tenemos un {elegido} en la lista")
