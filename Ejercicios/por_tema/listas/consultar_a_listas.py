a0e07c341ede0fb3ab259fa54c65797ed4576c30
VictorCancino <juanvictorcancino@gmail.com>
2024-03-12 11:11:18 -0600

    Se completo el ejercicio uno en el fichero 'consultar_a_listas.py'

---
+++ Ejercicios/por_tema/listas/consultar_a_listas.py
@@ -0,0 +1,5 @@
+#Ejercicio 1: Crea una lista con 10 profesiones y trae el valor que este en la posición 8
+
+profesiones = ["Arquitecto","Doctor","Programador","Diseñador","Dentista","Abogado", "Fisico", "Matematico","Deportista","Chef"]
+
+print(profesiones[8])
