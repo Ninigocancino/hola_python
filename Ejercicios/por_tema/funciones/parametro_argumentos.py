bb03f79055b71f0b4814696048fde0ba20ff2b52
VictorCancino <juanvictorcancino@gmail.com>
2024-03-15 11:50:16 -0600

    Se completo el ejercicio 1 en el fichero 'parametros_argumentos.py' en el directorio 'funciones'

---
+++ Ejercicios/por_tema/funciones/parametro_argumentos.py
@@ -0,0 +1,11 @@
+def calcular_area_cuadrada(lado):
+    """
+    Esta función calcula el área de un cuadrado
+    """
+
+    area = lado * lado
+    return area
+
+area_cuadrado = calcular_area_cuadrada(lado=5)
+
+print(area_cuadrado)
