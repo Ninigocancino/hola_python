0bd78b5708601039937da0dd076ae91421bbecbd
VictorCancino <juanvictorcancino@gmail.com>
2024-03-15 17:23:43 -0600

    Se completarón los ejercicios 1 y 2 del fichero 'break_continue.py'

---
+++ Ejercicios/por_tema/ciclo_for/break_continue.py
@@ -0,0 +1,15 @@
+#Ejercicio 1:
+nombres = ["Paco","Emilio","Mario"]
+
+for i in nombres:
+    print(i)
+    break
+
+#Ejercicio 2:
+
+for i in nombres:
+    if i == "Emilio":
+        break
+    print(i)
+
+
