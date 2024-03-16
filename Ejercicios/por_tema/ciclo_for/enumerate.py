2ef71e648a619dd4d86f9b2be85561fe516da186
VictorCancino <juanvictorcancino@gmail.com>
2024-03-15 19:55:08 -0600

    Se competó el ejercicio 2 en el fichero 'enumerate.py'

--- Ejercicios/por_tema/ciclo_for/enumerate.py
+++ Ejercicios/por_tema/ciclo_for/enumerate.py
@@ -8,3 +8,8 @@ nombres = ["Saul", "Alejandro", "Ana", "Emiliano"]
 nombres_enumerados = enumerate(nombres, start=5)
 print(nombres_enumerados)
 print(list(nombres_enumerados))
+
+#Ejercicio 2: Recorre cada elemento enumerado en el ejercicio anterior
+
+for indice, elemento in enumerate(nombres):
+    print(indice,elemento)
