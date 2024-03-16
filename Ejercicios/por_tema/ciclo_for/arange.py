9391ffe8339113b5b648831d2451bf5f1035a0b7
VictorCancino <juanvictorcancino@gmail.com>
2024-03-15 19:25:29 -0600

    Se completo el ejericio 1 en el fichero 'arange.py'

---
+++ Ejercicios/por_tema/ciclo_for/arange.py
@@ -0,0 +1,19 @@
+"""
+range(inicio,fin,paso)
+"""
+#Ejercicio 1:
+serie_1 = range(5)
+print(serie_1)
+print(list(serie_1))
+
+print("")
+
+serie_2 = range(5,10)
+print(serie_2)
+print(list(serie_2))
+
+print("")
+
+serie_3 = range(0,10,2)
+print(serie_3)
+print(list(serie_3))
