16e47f82565fa6e16bb7333082983e7cb4aba185
VictorCancino <juanvictorcancino@gmail.com>
2024-03-15 20:04:39 -0600

    Se completo el ejercicio 1 en el fichero 'for_else.py'

---
+++ Ejercicios/por_tema/ciclo_for/for_else.py
@@ -0,0 +1,10 @@
+#Ejercicio 1: 
+
+lista_nombres = ["Juan", "Agustin", "Elias", "Karla"]
+
+for nombre in lista_nombres:
+    print(nombre)
+    #if nombre == "Juan":
+        #break
+else:
+    print("se termina el ciclo")
