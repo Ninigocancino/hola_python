f6bfca6ef79b24ea5a1851279a1420c3b71b271b
VictorCancino <juanvictorcancino@gmail.com>
2024-03-15 19:40:29 -0600

    Se completó el ejercicio 2 en el fichero 'zip.py'

--- Ejercicios/por_tema/ciclo_for/zip.py
+++ Ejercicios/por_tema/ciclo_for/zip.py
@@ -6,3 +6,9 @@ apellidos = ["Gomez", "Luna", "Vaca"]
 nombre_completo = list(zip(nombres, apellidos))
 print(nombre_completo)
 
+#Ejercicio 2: Desempaqueta los elementos del ejercicio anterior
+
+nombres_unzip, apellidos_unzip = zip(*nombre_completo)
+print(nombres_unzip)
+print(apellidos_unzip)
+
