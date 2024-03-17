f88c68b855e68305142a0aea9fbc62850a391f96
VictorCancino <juanvictorcancino@gmail.com>
2024-03-16 19:36:39 -0600

    Se completó el ejercicio 1 del fichero 'crear_lista-con_condicionales.py'

---
+++ Ejercicios/por_tema/list_comprehension/crear_lista_con_condicionales.py
@@ -0,0 +1,17 @@
+"""
+lista = [expresion(elemento) for elemento in objeto_iterable if condicion]
+lista = [expresion(condicion) for elemento in objeto_iterable]
+"""
+#Ejercicio 1: Crea una expresión list comprehension donde se guarden los cuadrados de una lista dada y luego crea una list comprhension con una condición que imprima solo aquellos que cudadros cuyo numero original sea para
+def calcular_cuadrado(numero):
+    return numero**2
+
+def es_par(numero):
+    return numero % 2 == 0
+
+lista_num = [1,2,3,4,5,6,7,8,9,10]
+lista_cuadrados = [calcular_cuadrado(num) for num in lista_num]
+print(lista_cuadrados)
+
+lista_cuadrados_pares = [calcular_cuadrado(num) for num in lista_num if es_par(num)]
+print(lista_cuadrados_pares)
