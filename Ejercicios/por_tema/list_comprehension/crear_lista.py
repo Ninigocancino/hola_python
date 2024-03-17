d5e2412bce49ac135b7a0bf0019644da36a70f16
VictorCancino <juanvictorcancino@gmail.com>
2024-03-16 19:18:12 -0600

    Se completó el ejercicio 2 en el fichero 'crear_lista.py'

--- Ejercicios/por_tema/list_comprehension/crear_lista.py
+++ Ejercicios/por_tema/list_comprehension/crear_lista.py
@@ -4,7 +4,7 @@
 #Estructura de list comprehension:
 lista = [expresion(elemento) for elemento in objeto_iterable]
 """
-
+"""
 lista_num = [1,2,3,4,5,6,7,8,9,10]
 lista_cuadrados = []
 
@@ -16,4 +16,24 @@ print("Ciclo for", lista_cuadrados)
 
 
 list_comprehension = [num**2 for num in lista_num]
+print("list comprehension", list_comprehension)
+
+"""
+
+#Ejercicio 2: Toma como base el ejercicio anterior, pero ahora crea una función que sustituya en la expresion 'list comprehension' la instrucción de elevar al cuadrado los números de la lista inicial
+
+def elevar_cuadrado(num):
+    return num**2
+
+lista_num = [1,2,3,4,5,6,7,8,9,10]
+lista_cuadrados = []
+
+for num in lista_num:
+    cuadrado = num ** 2
+    lista_cuadrados.append(cuadrado)
+
+print("Ciclo for", lista_cuadrados)
+
+
+list_comprehension = [elevar_cuadrado(num) for num in lista_num]
 print("list comprehension", list_comprehension)
