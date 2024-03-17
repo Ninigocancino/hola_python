1eabb6982f8fa4a7a896db99df28cc13d1a68b3f
VictorCancino <juanvictorcancino@gmail.com>
2024-03-16 18:54:33 -0600

    Se completo el ejercicio 1 en el fichero 'map.py'

---
+++ Ejercicios/por_tema/list_comprehension/map.py
@@ -0,0 +1,18 @@
+def calcular_cuadrado(numero):
+    return numero**2
+
+lista_num = [1,2,3,4,5,6,7,8,9,10]
+lista_cuadrados = []
+
+for num in lista_num:
+#Agregando elementos a listas usando una función
+    
+ """
+    cuadrado = calcular_cuadrado(num)
+    lista_cuadrados(cuadrado)
+
+print(lista_cuadrados)
+"""
+
+map_cuadrados = list(map(calcular_cuadrado, lista_num))
+print(map_cuadrados)
