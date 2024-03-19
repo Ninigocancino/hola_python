55fa989e512d189a4f257a002901deea12eec60f
VictorCancino <juanvictorcancino@gmail.com>
2024-03-18 22:14:43 -0600

    Se completó el ejercicio 1 del fichero 'leer_csv.py'

---
+++ Ejercicios/por_tema/archivos_csv/leer_csv/leer_csv.py
@@ -0,0 +1,9 @@
+import csv
+
+
+#Ejercicio 1: Lee un archivo csv y muestralo en formato de lista en la consola
+with open("datos.csv", "r", encoding="UTF-8") as archivo:
+    reader = csv.reader(archivo)
+    next(reader) #esta instrucción evita que la primer fila del archivo se lea 
+    for fila in reader:
+        print(fila)
