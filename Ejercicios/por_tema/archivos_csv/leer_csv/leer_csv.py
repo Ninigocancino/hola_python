4c8fd136eaec4cfd90fb7b55a03b8337580064b7
VictorCancino <juanvictorcancino@gmail.com>
2024-03-18 22:19:25 -0600

    Se completó el ejercicio 3 en el fichero 'leer_csv.py'

--- Ejercicios/por_tema/archivos_csv/leer_csv/leer_csv.py
+++ Ejercicios/por_tema/archivos_csv/leer_csv/leer_csv.py
@@ -2,8 +2,18 @@ import csv
 
 
 #Ejercicio 1: Lee un archivo csv y muestralo en formato de lista en la consola
+"""
 with open("datos.csv", "r", encoding="UTF-8") as archivo:
     reader = csv.reader(archivo)
-    next(reader) #esta instrucción evita que la primer fila del archivo se lea 
+    #next(reader) #esta instrucción evita que la primer fila del archivo se lea 
+    for fila in reader:
+        print(fila)
+"""
+
+#Ejercicio 2: Lee un archivo csv y muestralo en formato de diccionario  en la consola
+        
+with open("datos.csv", "r", encoding="UTF-8") as archivo:
+    reader = csv.DictReader(archivo)
+    #next(reader) #esta instrucción evita que la primer fila del archivo se lea 
     for fila in reader:
         print(fila)
