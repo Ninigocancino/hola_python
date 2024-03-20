6db029de72c266d25876e7ba34fbbf653291c322
VictorCancino <juanvictorcancino@gmail.com>
2024-03-19 21:48:28 -0600

    Se completó el ejercicio 3 en el fichero 'formato_logs.py'

--- Ejercicios/por_tema/logging/formato_logs.py
+++ Ejercicios/por_tema/logging/formato_logs.py
@@ -1,8 +1,9 @@
 #Ejercicio 1: Crea 3 logs donde se imprima el nivel y el mensaje separado por un guión
 
-"""
+
 import logging
 
+"""
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -17,12 +18,25 @@ logging.critical("Log de error crítico")
 
 #Ejercicio 2: Toma el ejericico anterior y agrega al formato la fecha en que se produce el error 
 
-import logging
-
+"""
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logging.warning("Log de advertencia")
+logging.error("Log de error")
+logging.critical("Log de error crítico")
+
+"""
+
+#Ejercicio 3: Toma el ejericico anterior y cambia el formato en que se imprime la fecha del log para que se imprima la hora, minuto y segundos
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt= "%H:%M:%S"
 )
 
 logging.warning("Log de advertencia")
