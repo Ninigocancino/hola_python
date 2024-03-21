4d5ce0f95c9abad9d600b8bfedc1155dc3e2788a
VictorCancino <juanvictorcancino@gmail.com>
2024-03-20 20:58:23 -0600

    Se completó el ejercicio 1 en el fichero 'leer_json.py'

---
+++ Ejercicios/por_tema/archivos_json/leer_json.py
@@ -0,0 +1,9 @@
+import json
+
+#Ejercicio 1: crea un programa que deserialice (leer) un archivo json del directorio
+
+with open("persona.json", "r") as archivo_json:
+    datos_json = json.load(archivo_json)
+
+print(type(datos_json))
+print(datos_json["nombre"])
