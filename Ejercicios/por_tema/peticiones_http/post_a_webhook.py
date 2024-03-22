cf280b6c961dddfcf4685e750b50bfffa5cc368f
VictorCancino <juanvictorcancino@gmail.com>
2024-03-21 22:37:16 -0600

    Se creó el directorio 'debugging' y se agregó a él el fichero 'area_cuadrado.py'

--- Ejercicios/por_tema/peticiones_http/post_a_webhook.py
+++ Ejercicios/por_tema/peticiones_http/post_a_webhook.py
@@ -3,7 +3,15 @@ import requests
 #Ejercicio 1: Crea una petición que solicite datos al servidor
 url = "https://webhook.site/6dbbc723-ac21-4c6b-bf0b-1b01d1e2a8aa"
 
+"""
 payload = {"plato":"pasta","cantidad":2}
 response = requests.post(url, data=payload)
 print(response.status_code)
 print(response.text)
+"""
+
+#Ejercicio 2: Envia query params al servidor 
+
+payload = {"plato":"pasta","cantidad":2}
+query_params = {"nombre":"Florencio"}
+response = requests.post(url, data=payload, params=query_params)
