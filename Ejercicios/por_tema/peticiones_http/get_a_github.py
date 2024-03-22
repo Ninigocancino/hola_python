e457b06921de6e8213f0b93a5c38f4bb047af49b
VictorCancino <juanvictorcancino@gmail.com>
2024-03-21 19:00:01 -0600

    Se completó el ejercicio 2 del fichero 'get_a_github.py'

--- Ejercicios/por_tema/peticiones_http/get_a_github.py
+++ Ejercicios/por_tema/peticiones_http/get_a_github.py
@@ -3,3 +3,7 @@ import requests
 #Ejercicio 1: haz una etición GET a la API de GitHub e imprime el código de  estado de la petición 
 response = requests.get("https://api.github.com")
 print(response)
+
+#Ejercicio 2: Trae los headers de la petición 
+
+print(response.headers)
