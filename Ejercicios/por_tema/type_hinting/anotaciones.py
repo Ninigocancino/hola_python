6b1213ca458e3048f0dda3e4a9f3a139a1595182
VictorCancino <juanvictorcancino@gmail.com>
2024-04-02 23:38:57 -0600

    Se agregarón ejemplos de variables con anotaciones

---
+++ Ejercicios/por_tema/type_hinting/anotaciones.py
@@ -0,0 +1,11 @@
+nombre: str = "Ana"
+
+edad: int = 25
+
+pi: float = 3.1416
+
+apellidos: list[str] = ["Botero", "Tafur", "Quiñonez"]
+
+l_programacion: tuple[str,str,str,str] = ("python", "java", "javascript", "golang")
+
+lenguaje: dict[str,str] = {"nombre": "python", "creador": "Guido Van Rossum"}
